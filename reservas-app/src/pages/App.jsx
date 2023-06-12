22757470cabe04766c3ec2493f5fa8a5b7f5ba07
Ana Cruz <anmacruro4@gmail.com>
2023-06-11 19:40:15 -0500

    crud

--- reservas-app/src/pages/App.jsx
+++ reservas-app/src/pages/App.jsx
@@ -11,12 +11,95 @@ const App = () => {
   const [phone, setPhone] = useState(0)
   const [comments, setComments] = useState("")
 
+  const [formUpdate, setFormUpdate] = useState(false)
+  const [item, setItem] = useState(null)
+
+  const createUser = async() =>{
+    await addDoc(userCollectionRef, {nombre:name, correo:email, telefono:phone, comentario:comments})
+    getUsers()
+  }
+
+  const getUsers = async() =>{
+    const data = await getDocs(userCollectionRef)
+    setUser(data.docs.map((doc)=> ({...doc.data(), id: doc.id})))
+  }
+
+  const borrarUsuario = async(id) =>{
+    const userDoc = doc(db,'reservas',id)
+    await deleteDoc(userDoc)
+    getUsers()
+  }
+
+  const formUpdateOpen = (data) => {
+    setFormUpdate(true)
+    setItem(data)
+  }
+
+  const handleChange = (e) =>{
+    setItem({
+      ...item,
+      [e.target.name]: e.target.value
+    })
+    console.log(setItem)
+  }
+ 
+  const onUpdate = async(id) =>{
+    await updateDoc(doc(db,'reservas',id), item);
+    getUsers()
+  }
+
+  useEffect(() =>{
+    getUsers();
+  },[]);
 
   return (
     <div>
-      
+       <div className="form col-6">
+         <div className="mb-3">
+           <input type="text" placeholder='Nombre' className="form-control" onChange={(e) => {setName(e.target.value)}}/>
+         </div>
+         <div className="mb-3">
+           <input type="email" placeholder='Correo' className="form-control" onChange={(e) => {setEmail(e.target.value)}}/>
+         </div>
+         <div className="mb-3">
+           <input type="number" placeholder='Telefono' className="form-control" onChange={(e) => {setPhone(e.target.value)}}/>
+         </div>
+         <div className="mb-3">
+           <textarea placeholder='Comentarios' className="form-control" rows={3} defaultValue={""} onChange={(e) => {setComments(e.target.value)}}/>
+         </div>
+         <div className="d-grid gap-2 col-4 mx-auto">
+           <button id="btnCrear" className="btn btn-success" onclick={createUser()}>Enviar</button>
+         </div>
+       </div>
     </div>
+
+    {users.map((item)=>{
+      return(
+        <div key={item.id}>
+          <h5>Nombre: <p>{item.nombre}</p></h5>
+          <h5>Telefono: <p>{item.telefono}</p></h5>
+          <h5>Correo: <p>{item.correo}</p></h5>
+          <h5>Comentarios: <p>{item.comentarios}</p></h5>
+
+         <button className="btn btn-danger" onClick={()=> borrarUsuario(item.id)}>Borrar</button>
+         <button className="btn btn-warning" onClick={()=> formUpdateOpen(item)}>Editar</button>
+        </div>
+      )
+    })}
   )
+  
+    {
+      formUpdate &&
+      <>
+      <input type="text" placeholder='Nombre' value={item.nombre} name="nombre" onChange={handleChange}/>
+      <input type="text" placeholder='Correo' value={item.correo} name="correo" onChange={handleChange}/>
+      <input type="text" placeholder='Telefono' value={item.telefono} name="telefono" onChange={handleChange}/>
+      <input type="text" placeholder='Comentarios' value={item.comentarios} name="comentarios" onChange={handleChange}/>
+
+      <button className="btn btn-info" onClick={()=> onUpdate(item.id)}>Guardar</button>
+      </>
+    }
+
 }
 
 export default App
--- reservas-app/src/pages/main.jsx
+++ reservas-app/src/pages/main.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import '../style/index.css'
 
+<article className="first">
+  <div className="description col-6">
+    <div className="titles">
+      <h1 className="firtsTitle">Biacol</h1>
+      <h3 className="subtitle">del mar peruano a tu plato</h3>
+    </div>
+    <div className="text">
+      <p>Desde 1998 traemos para ti, tu familia y amigos, los mejores sabores de las familias colombianas.</p>
+    </div>
+  </div>            
+  <div className="imgPrincipal col-6">
+    <img src="https://casapropiacolombia.com/files/default/styles/webp_format/public/2020-12/foto-comida.jpg.webp?itok=L8OX-8JD" className="img-fluid" alt="comida-colombiana" />
+  </div>
+</article>
